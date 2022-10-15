87d031bb71dfb0df13f40d99dbd6535cb22b8cff
longyue0521 <longyueli0521@gmail.com>
2022-10-15 23:39:45 +0800

    以TODO注释形式描述测试用例

--- container/src/test/java/org/geektime/tdd/container/ContainerTest.java
+++ container/src/test/java/org/geektime/tdd/container/ContainerTest.java
@@ -1,14 +1,43 @@
 package org.geektime.tdd.container;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContainerTest {
 
-    @Test
-    public void should_fail() {
-        assertTrue(false);
+    interface Component {}
+    @Nested
+    class ComponentConstruction {
+        //TODO: instance
+        //TODO: abstract class
+        //TODO: interface
+        @Nested
+        class ConstructorInjection {
+            //TODO: No args constructor
+            //TODO: With Dependencies
+            //TODO: A -> B -> C
+        }
+
+        @Nested
+        class FieldInjection {
+
+        }
+
+        @Nested
+        class MethodInjection {
+
+        }
+
     }
 
+    @Nested
+    class DependenciesSelection {
+
+    }
+
+    @Nested
+    class LifecycleManagement {
+
+    }
 }
