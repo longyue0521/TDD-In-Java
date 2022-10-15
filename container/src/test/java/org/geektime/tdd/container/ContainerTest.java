7ba6a9e7b8a33a4bbd949b842024cb930587e219
longyue0521 <longyueli0521@gmail.com>
2022-10-15 23:42:26 +0800

    红灯：绑定到指定实例

--- container/src/test/java/org/geektime/tdd/container/ContainerTest.java
+++ container/src/test/java/org/geektime/tdd/container/ContainerTest.java
@@ -10,6 +10,16 @@ public class ContainerTest {
     @Nested
     class ComponentConstruction {
         //TODO: instance
+        @Test
+        void should_bind_type_to_a_specific_instance() {
+            Context context = new Context();
+
+            Component instance = new Component() {};
+            context.bind(Component.class, instance);
+
+            assertSame(instance, context.get(Component.class));
+        }
+
         //TODO: abstract class
         //TODO: interface
         @Nested
