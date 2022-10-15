9e7af6e113326e1ced0c5084eab1de2a35657b4f
longyue0521 <longyueli0521@gmail.com>
2022-10-15 23:43:24 +0800

    绿灯：创建Context类并实现相应方法以通过测试

---
+++ container/src/main/java/org/geektime/tdd/container/Context.java
@@ -0,0 +1,16 @@
+package org.geektime.tdd.container;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+
+    private Map<Class<?>, Object> components = new HashMap<>();
+    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
+        components.put(type, instance);
+    }
+
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        return (ComponentType) components.get(type);
+    }
+}
