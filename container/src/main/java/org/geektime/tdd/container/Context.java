77c0680b785354d06650645945481fe2a70bc029
longyue0521 <longyueli0521@gmail.com>
2022-10-17 00:03:30 +0800

    蓝灯：用Provider接口消除'代码重复'坏味道

--- container/src/main/java/org/geektime/tdd/container/Context.java
+++ container/src/main/java/org/geektime/tdd/container/Context.java
@@ -1,32 +1,30 @@
 package org.geektime.tdd.container;
 
-import java.lang.reflect.InvocationTargetException;
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
 
-    private Map<Class<?>, Object> components = new HashMap<>();
     private Map<Class<?>, Class<?>> componentImplementions = new HashMap<>();
+    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type, instance);
+        providers.put(type, (Provider<ComponentType>)() -> instance);
     }
 
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (components.containsKey(type)) {
-            return (ComponentType) components.get(type);
-        }
-        Class<?> implemention = componentImplementions.get(type);
-        try {
-            return (ComponentType) implemention.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return (ComponentType) providers.get(type).get();
     }
 
     public <ComponentType, ComponentImplementation extends ComponentType>
     void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        componentImplementions.put(type, implementation);
-
+        providers.put(type, (Provider<ComponentType>) () -> {
+            try {
+                return (ComponentType) ((Class<?>) implementation).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
